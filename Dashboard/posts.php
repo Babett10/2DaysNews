84e0c7994d782ec01585dea38fd05bde15438d68
HafizRaka77 <hafizraka14@gmail.com>
2023-11-24 09:34:30 +0700

    update file

--- Dashboard/posts.php
+++ Dashboard/posts.php
@@ -115,11 +115,11 @@
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="container">
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                        <h1 class="h2">Manga</h1>
+                        <h1 class="h2">Posts</h1>
                     </div>
 
                     <div class="table-responsive col-lg-11">
-                        <a href="komik/create.php" class="btn btn-primary mb-3">Create Manga</a>
+                        <a href="komik/create.php" class="btn btn-primary mb-3">Create Posts</a>
 
                         <table class="table table-striped table-sm">
                             <thead>
