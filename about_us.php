6ae0d2a14d60fe0de6ed1331904f564b14266265
HafizRaka77 <hafizraka14@gmail.com>
2023-12-08 18:33:44 +0700

    update new page

---
+++ about_us.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>2DAYNEWS | About Us</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="img/2daynews.png">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <div class="container-fluid p-0">
+            <nav class="navbar navbar-expand-lg bg-light navbar-light py-2 py-lg-0 px-lg-5">
+                <a href="" class="navbar-brand d-none d-lg-block">
+                    <h1 class="m-0 display-5 text-uppercase"><span class="text-primary">2Day</span>News</h1>
+                </a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between px-0 px-lg-3" id="navbarCollapse">
+                    <div class="navbar-nav mr-auto py-0">
+                        <a href="index.php" class="nav-item nav-link">Home</a>
+                        <a href="category.php" class="nav-item nav-link">Category</a>
+                        <a href="contact.php" class="nav-item nav-link">Contact</a>
+                        <a href="login.php" class="nav-item nav-link">Login</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+        <div class="container-fluid py-3 px-lg-5 mt-3">   
+            <div class="col-md-12 mt-3 mb-5">
+                <h2 style="text-align: center;">OUR TEAM</h2>
+            </div>
+            <div class="row mb-5">
+                <div class="col-lg-3 col-md-6 col-sm-12"> 
+                    <div class="card">
+                        <img src="img/team_05.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title" style="text-align: center;">Albert Radja Sihite</h5>
+                            <h6 class="card-subtitle text-muted" style="text-align: center;">Backend Developer</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12"> 
+                    <div class="card">
+                        <img src="img/team_05.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title" style="text-align: center;">Mohamad Egi Rahayu</h5>
+                            <h6 class="card-subtitle text-muted" style="text-align: center;">Backend Developer</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12"> 
+                    <div class="card">
+                        <img src="img/rk.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title" style="text-align: center;">Hafiz Raka Pradana</h5>
+                            <h6 class="card-subtitle text-muted" style="text-align: center;">Frontend Developer</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12"> 
+                    <div class="card">
+                        <img src="img/team_05.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title" style="text-align: center;">Anggun Wulan Sari</h5>
+                            <h6 class="card-subtitle text-muted" style="text-align: center;">Frontend Developer</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <footer>
+        <div class="container-fluid bg-light pt-5 px-sm-3 px-md-5">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 mb-5">
+                    <a href="index.php" class="navbar-brand">
+                        <h1 class="mb-2 mt-n2 display-5 text-uppercase"><span class="text-primary">2Day</span>News</h1>
+                    </a>
+                    <p>Dapatkan informasi berita terupdate dan terpopular serta akurat hanya disini</p>
+                    <div class="d-flex justify-content-start mt-4">
+                        <a class="btn btn-outline-secondary text-center mr-2 px-0" style="width: 38px; height: 38px;" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-outline-secondary text-center mr-2 px-0" style="width: 38px; height: 38px;" href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-secondary text-center mr-2 px-0" style="width: 38px; height: 38px;" href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="btn btn-outline-secondary text-center mr-2 px-0" style="width: 38px; height: 38px;" href="#"><i class="fab fa-instagram"></i></a>
+                        <a class="btn btn-outline-secondary text-center mr-2 px-0" style="width: 38px; height: 38px;" href="#"><i class="fab fa-youtube"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 mb-5 ml-auto">
+                    <h4 class="font-weight-bold mb-4">Media Partner</h4>
+                    <div class="d-flex flex-wrap m-n1">
+                        <a><img src="img/detik.png" alt="1" style="margin:5px;"></a>
+                        <a><img src="img/kompas.png" alt="2" style="margin:5px;"></a>
+                        <a><img src="img/liputan6.png" alt="3" style="margin:5px;"></a>
+                        <a><img src="img/cnn.png" alt="4" style="margin:5px;"></a>
+                        <a><img src="img/tribun.png" alt="5" style="margin:5px;"></a>
+                        <a><img src="img/cnbc.png" alt="6" style="margin:5px;"></a>
+                        <a><img src="img/bola1.png" alt="14" style="margin:5px;"></a>
+                        <a><img src="img/bola.png" alt="7" style="margin:5px;"></a>
+                        <a><img src="img/goal.png" alt="8" style="margin:5px;"></a>
+                        <a><img src="img/tempo.png" alt="9" style="margin:5px;"></a>
+                        <a><img src="img/times.png" alt="10" style="margin:5px;"></a>
+                        <a><img src="img/jpnn.png" alt="11" style="margin:5px;"></a>
+                        <a><img src="img/jawapos.png" alt="12" style="margin:5px;"></a>
+                        <a><img src="img/okzone.png" alt="13" style="margin:5px;"></a>
+                        <a><img src="img/suara.png" alt="14" style="margin:5px;"></a>
+                        <a><img src="img/kumparan.png" alt="14" style="margin:5px;"></a>
+                        <a><img src="img/sindonews.png" alt="14" style="margin:5px;"></a>
+                        <a><img src="img/idn.png" alt="14" style="margin:5px;"></a>
+                        <a><img src="img/INews.png" alt="14" style="margin:5px;"></a>
+                        <a><img src="img/merdeka.png" alt="14" style="margin:5px;"></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 mb-5 ml-auto">
+                    <h4 class="font-weight-bold mb-4">Quick Links</h4>
+                    <div class="d-flex flex-column justify-content-start">
+                        <a class="text-secondary" href="index.php"><i class="fa fa-angle-right text-dark mr-2"></i>Home</a>
+                        <a class="text-secondary" href="category.php"><i class="fa fa-angle-right text-dark mr-2"></i>Category</a>
+                        <a class="text-secondary" href="contact.php"><i class="fa fa-angle-right text-dark mr-2"></i>Contact</a>
+                        <a class="text-secondary" href="about_us.php"><i class="fa fa-angle-right text-dark mr-2"></i>About us</a>
+                        <a class="text-secondary" href="login.php"><i class="fa fa-angle-right text-dark mr-2"></i>Login</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid py-4 px-sm-3 px-md-5">
+            <p class="m-0 text-center">
+                &copy; <a class="font-weight-bold" href="#">2DAYNEWS</a>. All Rights Reserved.
+
+                <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                Designed by <a class="font-weight-bold" href="https://github.com/Babett10/FinalProject">Kelompok 2</a>
+            </p>
+        </div>
+    </footer>
+    <!-- Footer End -->
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-dark back-to-top"><i class="fa fa-angle-up"></i></a>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="mail/jqBootstrapValidation.min.js"></script>
+    <script src="mail/contact.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+    <script type="text/javascript">
+
+        function observer() {
+            const nav = document.querySelector(".nav-observer")
+            const intersection = new IntersectionObserver((entries) => {
+                const [entry] = entries
+
+                if (entry.isIntersecting) {
+                    document.querySelector(".navbar").classList.remove("fixed-top")
+                } else {
+                    document.querySelector(".navbar").classList.add("fixed-top")
+                }
+            }, {
+                threshold: 1,
+            })
+
+            intersection.observe(nav)
+        }
+    </script>
+</body>
+
+</html>
